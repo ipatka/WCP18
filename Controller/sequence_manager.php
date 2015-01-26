bc9d0ffd592ed47135817a8b2ad0a5438d9f077b
Isaac Patka <ipatka1@binghamton.edu>
2015-01-25 21:59:13 -0500

    added javascript to traverse table. need help creating the object

---
+++ Controller/sequence_manager.php
@@ -0,0 +1,10 @@
+<?php
+
+// recieve posts of the sequence object from new_seqence.js
+// insert it into the database
+
+
+// recieve posts of a sequence id from home.js
+// retrieve sequence from database
+// echo queue order and length of time to execute
+// execute sequence
