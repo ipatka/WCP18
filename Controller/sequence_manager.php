ca0ba228e3d9cc0ea21cbabdbef1c5e7bf3aca5a
Isaac Patka <ipatka1@binghamton.edu>
2015-01-26 18:38:50 -0500

    object gets posted to sequence_manager. needs to be inserted into database

--- Controller/sequence_manager.php
+++ Controller/sequence_manager.php
@@ -1,4 +1,6 @@
+
 <?php
+
 
 // recieve posts of the sequence object from new_seqence.js
 // insert it into the database
@@ -8,3 +10,49 @@
 // retrieve sequence from database
 // echo queue order and length of time to execute
 // execute sequence
+
+include 'Base.php';
+
+class Controller_Sequence_Manager extends Controller_Base {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function route() {
+        
+
+        $sequence_array = $_POST["sequence_post"];
+        $frame = 1;
+        foreach ($sequence_array as $key => $value) {
+            $entry = 1;
+            echo json_encode("frame");
+            echo json_encode($frame);
+            foreach ($value as $sub_array => $value_b) {
+                if ($entry < 9) {
+                    echo json_encode("nozzle"), json_encode($entry);
+                    echo json_encode("state"), json_encode($value_b);
+
+                }
+                else {
+                    echo json_encode("frame_length"), json_encode($value_b);
+                }
+
+
+                $entry++;
+            }
+            echo "\n";
+            $frame++;
+        }
+
+        
+
+    }
+}
+
+$sequences = new Controller_Sequence_Manager();
+$sequences->route();
+
+?>
+
+
--- Javascript/new_sequence.js
+++ Javascript/new_sequence.js
@@ -14,44 +14,68 @@ $(document).ready(function(){
 
 
 	$(document).on("click","#submit", function() {
-		console.log("submit");
 		var object_to_post = create_sequence_object();
-
+		$.post('/Controller/sequence_manager.php',{
+			sequence_post: object_to_post
+		}
+		,function(data) {
+				console.log("posted. here's the data returned: "+data);
+			});
 		//post the object to /Controller/sequence_manager.php
 	});
 
 
 
 function create_sequence_object() {
-	console.log("creating sequence object");
 
-	var sequence_object = {};
+	var rows = 20;
+	var new_sequence = createArray(rows,9);
+	// sequence_object.new_sequence[8][6] = 6;
+	//console.log("array "+sequence_object.new_sequence);
 
 	$(".row").each(function(i) {
 
-		$(this).find(".valve_button").each(function() {
+		$(this).find(".valve_button").each(function(j) {
 			//initialize the array
 			if ($(this).hasClass("valve_selected")) {
 				var row = $(this).closest('.row').attr('id');
 				console.log(row);
 				console.log(this.id);
+				new_sequence[i][j] = 1;
 
 				//insert 'on' into the array
 
 			}
 			else{
 				//insert 'off' into the array
+				new_sequence[i][j] = 0;
 			}
+			// frame length
+			var length = $(this).parents('.row').find('.frame_length').val();
+			new_sequence[i][8] = length;
 
-				//insert into the sequence_object the row id, an array of on/off and the frame length
 
 		});
 	});
 
-	return sequence_object;
+	console.log(new_sequence);
+
+	return new_sequence;
 
 
 
+}
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
 }
 
 
