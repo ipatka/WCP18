08da3c6dd4bb8bfcc32d9e1e3b8fb244a67620ed
Isaac Patka <ipatka1@binghamton.edu>
2015-02-09 10:53:35 -0500

    should work with all 8 leds

--- Controller/sequence_manager.php
+++ Controller/sequence_manager.php
@@ -21,7 +21,7 @@ class Controller_Sequence_Manager extends Controller_Base {
 
     public function route() {
         
-	
+	    $interpret = array(4 , 17 , 27 , 22 , 18 , 23 , 24 , 25);
         $sequence_array = $_POST["sequence_post"];
         $frame = 1;
         foreach ($sequence_array as $key => $value) {
@@ -30,6 +30,9 @@ class Controller_Sequence_Manager extends Controller_Base {
             echo json_encode($frame);
             foreach ($value as $sub_array => $value_b) {
                 if ($entry < 9) {
+                    if ($value_b == 1) {
+                        $pins[$entry - 1] = $interpret[$entry - 1];
+                    }
                     echo json_encode("nozzle"), json_encode($entry);
                     echo json_encode("state"), json_encode($value_b);
 
@@ -44,9 +47,9 @@ class Controller_Sequence_Manager extends Controller_Base {
                 $entry++;
             }
 
-            $pins[0] = 17;
-            $pins[1] = 27;
-		exec('sudo ./../external_libraries/php-blinker/myBlinker "' . serialize($pins) . '" "' . addslashes($frame_length) . '"');
+            // $pins[0] = 17;
+            // $pins[1] = 27;
+		  exec('sudo ./../external_libraries/php-blinker/myBlinker "' . serialize($pins) . '" "' . addslashes($frame_length) . '"');
             echo "\n";
             $frame++;
         }
