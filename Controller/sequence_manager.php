ca036bbe6dd42089331e1d6936dcc90836ad4413
Isaac Patka <ipatka1@binghamton.edu>
2015-02-09 11:13:38 -0500

    trying to clear pins

--- Controller/sequence_manager.php
+++ Controller/sequence_manager.php
@@ -52,7 +52,9 @@ class Controller_Sequence_Manager extends Controller_Base {
             // $pins[0] = 17;
             // $pins[1] = 27;
 		  exec('sudo ./../external_libraries/php-blinker/myBlinker "' . serialize($pins) . '" "' . addslashes($frame_length) . '"');
-            $pins = 0;
+            foreach ($pins as &$value) {
+                $value = null;
+            }
             echo "\n";
             $frame++;
         }
